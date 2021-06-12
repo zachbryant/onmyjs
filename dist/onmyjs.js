7e0f3c7f6f5f2341f6da6e3ea27b73a899e35d0e
Zach Bryant <zdb282@gmail.com>
2021-06-12 03:34:12 -0700

    better export & relative paths

--- dist/onmyjs.js
+++ dist/onmyjs.js
@@ -1,16 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.onmyjs = void 0;
 /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
 const fs = require('fs');
 const path = require('path');
 const callerPath = require('caller-path');
-function onmyjs(data, outputFilename, pretty = true) {
+function onmyjs(data, outputPath, pretty = true) {
     const caller = callerPath();
-    const directory = path.dirname(caller);
-    const _outputFilebase = outputFilename ?? path.basename(caller);
+    const callerDirectory = path.dirname(caller);
+    const _outputFilebase = path.basename(caller);
     const _outputFilename = path.parse(_outputFilebase).name;
-    const outPath = path.resolve(directory, `${_outputFilename}.json`);
+    const outPath = path.resolve(callerDirectory, outputPath ?? `${_outputFilename}.json`);
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
     fs.writeFileSync(`${outPath}`, JSON.stringify(data, null, pretty ? 2 : 0));
 }
-exports.default = onmyjs;
+exports.onmyjs = onmyjs;
--- onmyjs.ts
+++ onmyjs.ts
@@ -3,18 +3,16 @@ const fs = require('fs');
 const path = require('path');
 const callerPath = require('caller-path');
 
-export default function onmyjs(
-	data: Record<string, any>,
-	outputFilename?: string,
-	pretty = true
-): void {
+function onmyjs(data: Record<string, any>, outputPath?: string, pretty = true): void {
 	const caller = callerPath();
-	const directory = path.dirname(caller);
-	const _outputFilebase = outputFilename ?? path.basename(caller);
+	const callerDirectory = path.dirname(caller);
+	const _outputFilebase = path.basename(caller);
 	const _outputFilename = path.parse(_outputFilebase).name;
 
-	const outPath = path.resolve(directory, `${_outputFilename}.json`);
+	const outPath = path.resolve(callerDirectory, outputPath ?? `${_outputFilename}.json`);
 
 	// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
 	fs.writeFileSync(`${outPath}`, JSON.stringify(data, null, pretty ? 2 : 0));
 }
+
+export { onmyjs };
